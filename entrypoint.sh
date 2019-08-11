ed2c2aeed3d38f4e39dc871b7fb6b8709bcffa34
Evin Callahan <evin.callahan@gmail.com>
2019-08-10 20:31:42 -0500

    config startup (#2)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ FLEXGET_PASSWORD="${FLEXGET_PASSWORD:-default password}"
 
 confd -onetime -backend env
 
+# on startup make sure that we get rid of the lock file as it prevents startup
+LOCKFILE=/config/.config-lock
+test -f $LOCKFILE && rm $LOCKFILE
+
 flexget web passwd "$FLEXGET_PASSWORD"
 
 exec "$@"
